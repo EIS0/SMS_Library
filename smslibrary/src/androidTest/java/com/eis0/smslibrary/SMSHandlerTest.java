a082c1bb04b5c2d460cfe5878317d800d7462fcd
Edoardo Raimondi <56430825+EdoardoRaimondi@users.noreply.github.com>
2019-11-04 21:40:22 +0100

    SMSHandler test class added (one method)

---
+++ smslibrary/src/androidTest/java/com/eis0/smslibrary/SMSHandlerTest.java
@@ -0,0 +1,17 @@
+package com.eis0.smslibrary;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SMSHandlerTest {
+
+    @Test
+    public void isPendingMessagesEmpty() {
+            boolean result;
+            SMSHandler.addReceiveListener(null);
+            result = SMSHandler.isPendingMessagesEmpty();
+            assertEquals(result, true);
+
+        }
+    }
