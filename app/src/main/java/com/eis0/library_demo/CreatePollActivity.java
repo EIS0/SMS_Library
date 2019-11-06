ad737b3e7f0088801746ef51014e8e5832f302ed
Cogno_Marco <marco.cognolato.98@gmail.com>
2019-11-06 16:04:05 +0100

    Created Basic Activity setup to send polls

---
+++ app/src/main/java/com/eis0/library_demo/CreatePollActivity.java
@@ -0,0 +1,14 @@
+package com.eis0.library_demo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreatePollActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_poll);
+    }
+}
