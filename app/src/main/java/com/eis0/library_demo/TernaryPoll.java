8f7a4cf6280999fd508d7005061b3c101084e954
giovannivelludo <56405609+giovannivelludo@users.noreply.github.com>
2019-11-07 19:53:24 +0100

    added methods to create message from new poll and to send said message, still need to add instance variable pollAuthor

--- app/src/main/java/com/eis0/library_demo/TernaryPoll.java
+++ app/src/main/java/com/eis0/library_demo/TernaryPoll.java
@@ -13,10 +13,11 @@ import java.util.Map;
 class TernaryPoll extends Poll {
     private enum PollResult {YES, NO, UNAVAILABLE}
     private static int pollCount = 0;
+    // TODO: check if there can be pollId conflicts when polls are created by different users
     private int pollId;
     private String pollQuestion;
     private Map<SMSPeer, PollResult> pollUsers;
-    private final static String LOG_KEY = "APP_POLL";
+    private static final String LOG_KEY = "APP_POLL";
 
     TernaryPoll() {
         pollId = ++TernaryPoll.pollCount;
@@ -28,17 +29,14 @@ class TernaryPoll extends Poll {
      * Creates a poll and sends it to all users included in it
      * @param question the question to ask all users
      * @param users users to include in the poll
-     * @param context context of the application to use when needed, needed by SMSManager
+     * @param activity context of the application creating the poll, needed by SMSManager
      */
-    TernaryPoll(String question, SMSPeer[] users, Context context) {
+    TernaryPoll(String question, SMSPeer[] users, Context activity) {
         pollId = ++TernaryPoll.pollCount;
         pollQuestion = question;
         pollUsers = new HashMap<>();
         for (SMSPeer user : users) this.addUser(user);
-        for (SMSPeer user : pollUsers.keySet()) {
-            // TODO: send Poll instead of empty message
-            SMSManager.getInstance(context).sendMessage(new SMSMessage(user, ""));
-        }
+        this.sendPoll(activity);
     }
 
     /**
@@ -89,5 +87,35 @@ class TernaryPoll extends Poll {
      */
     int getPollId() {
         return this.pollId;
+    }
+
+    /**
+     * Converts a new poll to the following String:
+     * messageCode + pollAuthor + pollId + pollQuestion + pollUsers
+     * Fields are separated by the character CR, except for messageCode
+     * and pollAuthor because the first is always only the first character.
+     * Various pollUsers are separated by the character CR.
+     *
+     * messageCode assumes the following values:
+     * 0 when the message contains a new poll
+     * 1 when the message is sent from a user to the author and contains an answer
+     * 2 when the message is sent from the author to users and contains updated poll data
+     *
+     * @return message to send to poll users
+     */
+    private String pollToMessage() {
+        String message = "0" + pollAuthor + "\r" + pollId + "\r" + pollQuestion;
+        // adds each pollUser to the end of the message
+        for (SMSPeer user : pollUsers.keySet()) {
+            message = message + "\r" + user.getAddress();
+        }
+        return message;
+    }
+
+    private void sendPoll(Context activity) {
+        String message = this.pollToMessage();
+        for (SMSPeer user : pollUsers.keySet()) {
+            SMSManager.getInstance(activity).sendMessage(new SMSMessage(user, message));
+        }
     }
 }
