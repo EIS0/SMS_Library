b399a9c097c7add8162ed1f14237b112bc20b2a9
giovannivelludo <56405609+giovannivelludo@users.noreply.github.com>
2019-11-08 00:32:42 +0100

    changed packet headers, started writing method to parse incoming messages

--- app/src/main/java/com/eis0/library_demo/PacketHandler.java
+++ app/src/main/java/com/eis0/library_demo/PacketHandler.java
@@ -6,11 +6,14 @@ import com.eis0.smslibrary.SMSPeer;
 
 // TODO: add checks for lost packages during the transmission of messages
 // TODO: check what happens if characters outside of the GSM character set are used
+// TODO: handle messages too large to be split in the maximum amount of fragments allowed in an SMS
+// TODO: skip check of TOTAL_FRAGMENTS in messages other than the first
 class PacketHandler implements ReceivedMessageListener<SMSMessage> {
 
-    /**
+    /*
+     * Messages longer than (140 chars - packet headers) are split into fragments.
      * Packet structure is the following:
-     * APP_ID | MESSAGE_NUMBER | TOTAL FRAGMENTS | FRAGMENT_NUMBER | PAYLOAD
+     * APP_ID | MESSAGE_NUMBER | FRAGMENT_NUMBER | [TOTAL FRAGMENTS] | PAYLOAD
      * Fields are separated by the CR character, except for the first two because they have fixed
      * lengths.
      *
@@ -18,10 +21,21 @@ class PacketHandler implements ReceivedMessageListener<SMSMessage> {
      * to our app;
      * MESSAGE_NUMBER identifies to which message this fragment belongs, together with the sender's
      * phone number. It's a single char, it allows us to distinguish a message from others with
-     * reasonable certainty;
-     * TOTAL_FRAGMENTS is the number of fragments in which the original message was split;
-     * FRAGMENT_NUMBER identifies fragments in an orderly manner.
+     * reasonable confidence;
+     * FRAGMENT_NUMBER identifies fragments in an orderly manner;
+     * TOTAL_FRAGMENTS is the number of fragments in which the original message was split, it is
+     * only sent in the first fragment of the message;
+     * PAYLOAD is the message fragment.
      */
+
+    private static final int messageNumberIndex = 1;
+    private static final int fragmentNumberIndex = 2;
+    private static int totalFragmentsIndex;
+    private static int payloadIndex;
+    private int messageNumber;
+    private int fragmentNumber;
+    private int totalFragments;
+    private String payload;
 
     /**
      * Called by SMSHandler whenever an SMS message with our APP_ID is received.
@@ -30,7 +44,29 @@ class PacketHandler implements ReceivedMessageListener<SMSMessage> {
     public void onMessageReceived(SMSMessage message) {
         SMSPeer sender = message.getPeer();
         String data = message.getData();
-        int messageNumber = Character.getNumericValue(data.charAt(1));
+
+        // parse fields from SMS text
+        int messageNumber = Character.getNumericValue(data.charAt(messageNumberIndex));
+        String fragmentNumberString = "";
+        for (int i = fragmentNumberIndex; i < data.length(); i++) {
+            char c = data.charAt(i);
+            if (data.charAt(i) != '\r') fragmentNumberString += c;
+            else {
+                fragmentNumber = Integer.parseInt(fragmentNumberString);
+                if (fragmentNumber == 1) {
+                    totalFragmentsIndex = i + 1;
+                    break;
+                } else {
+                    payloadIndex = i + 1;
+                    break;
+                }
+            }
+        }
+        if (fragmentNumber == 1) {
+            // parse totalFragments
+        }
+        // parse payload
+
         // TODO: complete this method
     }
 
