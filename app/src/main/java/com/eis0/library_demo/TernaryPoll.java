ae5ea99af3668647550d6a24f34d1597d9f2480f
giovannivelludo <56405609+giovannivelludo@users.noreply.github.com>
2019-11-07 13:23:36 +0100

    more informative class name, non-empty constructor now sends empty message to all users in the poll, Map pollUsers now uses SMSPeer type for keys instead of String SMSPeer.address

--- app/src/main/java/com/eis0/library_demo/TernaryPoll.java
+++ app/src/main/java/com/eis0/library_demo/TernaryPoll.java
@@ -1,32 +1,43 @@
 package com.eis0.library_demo;
 
+import android.content.Context;
 import android.util.Log;
 
+import com.eis0.smslibrary.SMSManager;
+import com.eis0.smslibrary.SMSMessage;
 import com.eis0.smslibrary.SMSPeer;
 
 import java.util.HashMap;
 import java.util.Map;
 
-class BinaryPoll extends Poll {
+class TernaryPoll extends Poll {
     private enum PollResult {YES, NO, UNAVAILABLE}
     private static int pollCount = 0;
     private int pollId;
     private String pollQuestion;
-    private Map<String, PollResult> pollUsers;
+    private Map<SMSPeer, PollResult> pollUsers;
     private final static String LOG_KEY = "APP_POLL";
 
-    BinaryPoll () {
-        pollId = ++BinaryPoll.pollCount;
+    TernaryPoll() {
+        pollId = ++TernaryPoll.pollCount;
         pollQuestion = "";
         pollUsers = new HashMap<>();
     }
 
-    BinaryPoll (String question, SMSPeer[] users) {
-        pollId = ++BinaryPoll.pollCount;
+    /**
+     * Creates a poll and sends it to all users included in it
+     * @param question the question to ask all users
+     * @param users users to include in the poll
+     * @param context context of the application to use when needed, needed by SMSManager
+     */
+    TernaryPoll(String question, SMSPeer[] users, Context context) {
+        pollId = ++TernaryPoll.pollCount;
         pollQuestion = question;
         pollUsers = new HashMap<>();
-        for (SMSPeer user:users) {
-            this.addUser(user);
+        for (SMSPeer user : users) this.addUser(user);
+        for (SMSPeer user : pollUsers.keySet()) {
+            // TODO: send Poll instead of empty message
+            SMSManager.getInstance(context).sendMessage(new SMSMessage(user, ""));
         }
     }
 
@@ -36,7 +47,7 @@ class BinaryPoll extends Poll {
      * @return true if the user is in the poll, false otherwise
      */
     boolean hasUser(SMSPeer user){
-        return pollUsers.containsKey(user.getAddress());
+        return pollUsers.containsKey(user);
     }
 
     /**
@@ -46,7 +57,7 @@ class BinaryPoll extends Poll {
     void addUser(SMSPeer user){
         // at the beginning we have no feedback by the user
         PollResult result = PollResult.UNAVAILABLE;
-        pollUsers.put(user.getAddress(), result);
+        pollUsers.put(user, result);
     }
 
     /**
@@ -56,7 +67,7 @@ class BinaryPoll extends Poll {
     void setYes(SMSPeer user) {
         if (hasUser(user)) {
             PollResult result = PollResult.YES;
-            pollUsers.put(user.getAddress(), result);
+            pollUsers.put(user, result);
         }
         else Log.i(LOG_KEY, "trying to manage an inexistent user");
     }
@@ -68,7 +79,7 @@ class BinaryPoll extends Poll {
     void setNo(SMSPeer user) {
         if (hasUser(user)) {
             PollResult result = PollResult.NO;
-            pollUsers.put(user.getAddress(), result);
+            pollUsers.put(user, result);
         }
         else Log.i(LOG_KEY, "trying to manage an inexistent user");
     }
