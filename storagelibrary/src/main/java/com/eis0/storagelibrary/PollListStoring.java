6a049738d088842dd50d2605bdafbc89eb5b0687
Enrico Cestaro <enrico.cestaro.3@studenti.unipd.it>
2019-11-12 21:45:31 +0100

    Added descriptions
    Added more descriptions of the various methods of the PollListStoring class

--- storagelibrary/src/main/java/com/eis0/storagelibrary/PollListStoring.java
+++ storagelibrary/src/main/java/com/eis0/storagelibrary/PollListStoring.java
@@ -1,6 +1,7 @@
 /**
- * This class allows the user to save, load and modify three different lists containing TernaryPoll
- * objects
+ * This class allows the user to save, load and modify three different lists containing the names of
+ * the file saved in the Internal Storage
+ * The three lists differ only for their ultimate purpose
  *
  * @author Enrico Cestaro
  */
@@ -16,30 +17,39 @@ import java.util.ArrayList;
 
 public class PollListStoring extends StoringJsons implements JsonConverter<ArrayList<String>> {
 
+    //List for the pending polls, waiting to be closed
     public final String pendingListName = "pendingPollList.json";
     private ArrayList<String> pendingPollList;
 
+    //List for the closed polls
     public final String closedListName = "closedPollList.json";
     private ArrayList<String> closedPollList;
 
+    //List for the received polls
     public final String receivedListName = "receivedPollList.json";
     private ArrayList<String> receivedPollList;
 
-    /**TODO Make it better
-     *
+
+    /**
+     * TODO: add the pollIdCounter to a file to save in the Internal Memory
+     * Right now, it's only possible to modify it, it still get reset when the app is closed
      */
     private static int pollIdCounter;
+
     public void setPollIdCounter(int pollIdCounter) {
-    this.pollIdCounter = pollIdCounter;
+        this.pollIdCounter = pollIdCounter;
     }
+
     public int getPollIdCounter() {
         return pollIdCounter;
     }
 
-
     /**
-     * @param listOfFiles
-     * @return
+     * This method converts an ArrayList<String> object into the corresponding .json file
+     *
+     * @param listOfFiles The ArrayList<String> object with the names of the files containing
+     *                    the polls
+     * @return returns a String value
      */
     public String convertToJson(ArrayList<String> listOfFiles) {
         Gson gson = new Gson();
@@ -49,8 +59,10 @@ public class PollListStoring extends StoringJsons implements JsonConverter<Array
     }
 
     /**
-     * @param json
-     * @return
+     * This method converts a .json file into the corresponding ArrayList<String> object
+     *
+     * @param json the String value of the .json file to convert
+     * @return Returns an ArrayList<String> object
      */
     public ArrayList<String> convertFromJson(String json) {
         Gson gson = new Gson();
@@ -60,9 +72,11 @@ public class PollListStoring extends StoringJsons implements JsonConverter<Array
     }
 
     /**
+     * This method saves the specified ArrayList<String> object with the specified listName
+     *
      * @param context
-     * @param listName
-     * @param pollList
+     * @param listName The name of the list of files to save in the Internal Storage
+     * @param pollList The list to save in the Internal Storage
      */
     public void savePollList(Context context, String listName, ArrayList<String> pollList) {
         String json = this.convertToJson(pollList);
@@ -70,8 +84,10 @@ public class PollListStoring extends StoringJsons implements JsonConverter<Array
     }
 
     /**
+     * This method loads the ArrayList<String> object with the corresponding specified listName
+     *
      * @param context
-     * @param fileName
+     * @param fileName The name of the ArrayList<String> object to load from the Internal Storage
      * @return
      */
     public ArrayList<String> loadPollList(Context context, String fileName) {
@@ -80,8 +96,10 @@ public class PollListStoring extends StoringJsons implements JsonConverter<Array
     }
 
     /**
-     * @param listName
-     * @param fileName
+     * This method removes from the specified list the String value equal to the fileName value inserted
+     *
+     * @param listName name of the list to modify
+     * @param fileName String value to remove
      */
     public void removeFromPollList(String listName, String fileName) {
         switch (listName) {
@@ -100,8 +118,10 @@ public class PollListStoring extends StoringJsons implements JsonConverter<Array
     }
 
     /**
-     * @param listName
-     * @param fileName
+     * This method adds to the specified list the String value equal to the fileName value inserted
+     *
+     * @param listName name of the list to modify
+     * @param fileName String value to add
      */
     public void addToPollList(String listName, String fileName) {
         switch (listName) {
