baf42a926538272178b4a5d08338141cb3c6d266
giovannivelludo <56405609+giovannivelludo@users.noreply.github.com>
2019-11-13 16:19:51 +0100

    completed test of onMessageReceived(), limited to case where new poll is received

--- app/src/test/java/com/eis0/library_demo/PollManagerTest.java
+++ app/src/test/java/com/eis0/library_demo/PollManagerTest.java
@@ -6,15 +6,19 @@ import androidx.test.core.app.ApplicationProvider;
 import com.eis0.smslibrary.SMSMessage;
 import com.eis0.smslibrary.SMSPeer;
 
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class PollManagerTest {
-    private Context fakeContext;
-    private PollManager pollManager;
+import java.util.ArrayList;
 
-    @Before
-    public void setUp() {
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PollManagerTest implements PollListener {
+    private static Context fakeContext;
+    private static PollManager pollManager;
+
+    @BeforeClass
+    public static void classSetUp() {
         fakeContext = ApplicationProvider.getApplicationContext();
         pollManager = PollManager.getInstance(fakeContext);
     }
@@ -28,13 +32,34 @@ public class PollManagerTest {
     }
 
     @Test
-    public void onNewPollReceived() {
+    public void onMessageReceived() {
+        // CASE 0: received new poll
+
+        // registers listener which will be called by PollManager
+        pollManager.addPollListener(this);
         SMSPeer sender = new SMSPeer("3337235485");
-        String text = "03331727754\r38\rQuestion\r3493619544\r3335436782\r+396662838864";
+        String text = "03337235485\r38\rQuestion\r3493619544\r3335436782\r+396662838864";
         SMSMessage message = new SMSMessage(sender, text);
         pollManager.onMessageReceived(message);
+        /* TODO: assert if onNewPollReceived() is being called (with Mockito?). If there's a way of
+         *  doing it, then remove the implementation of PollListener and its methods from this class
+         */
+    }
 
+    @Override
+    public void onNewPollReceived(TernaryPoll poll) {
+        ArrayList<SMSPeer> users = new ArrayList<>();
+        users.add(new SMSPeer("3493619544"));
+        users.add(new SMSPeer("3335436782"));
+        users.add(new SMSPeer("+396662838864"));
+        TernaryPoll verificationPoll =
+                new TernaryPoll("Question", new SMSPeer("3337235485"),
+                        users, 38);
+        assertThat(poll).isEqualTo(verificationPoll);
+    }
 
+    @Override
+    public void onPollUpdated(TernaryPoll poll) {
 
     }
 }
