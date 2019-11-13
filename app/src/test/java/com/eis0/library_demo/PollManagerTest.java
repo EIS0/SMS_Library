6b98004df593032856d8b12f3f034704d3fa83aa
giovannivelludo <56405609+giovannivelludo@users.noreply.github.com>
2019-11-13 15:24:43 +0100

    moved TernaryPollTest from app/src/androidTest to app/src/rest; started writing tests for PollManager

--- app/src/test/java/com/eis0/library_demo/PollManagerTest.java
+++ app/src/test/java/com/eis0/library_demo/PollManagerTest.java
@@ -3,9 +3,13 @@ package com.eis0.library_demo;
 import android.content.Context;
 import androidx.test.core.app.ApplicationProvider;
 
+import com.eis0.smslibrary.SMSMessage;
+import com.eis0.smslibrary.SMSPeer;
+
 import org.junit.Test;
 
 public class PollManagerTest {
+    private Context context = ApplicationProvider.getApplicationContext();
 
     @Test
     public void createPoll() {
@@ -17,9 +21,9 @@ public class PollManagerTest {
 
     @Test
     public void onMessageReceived() {
-        private Context context = ApplicationProvider.getApplicationContext();
         PollManager pollManager = PollManager.getInstance(context);
-        String message = "";
+        String text = "03331727754\r38\rQuestion\r3493619544"
+        SMSMessage message = new SMSMessage(new SMSPeer("3337235485"), text);
         pollManager.onMessageReceived(message);
 
 
