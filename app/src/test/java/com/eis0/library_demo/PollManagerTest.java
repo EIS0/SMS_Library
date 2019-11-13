a81357816c95be5b3f7e2ae668971194f150d4de
giovannivelludo <56405609+giovannivelludo@users.noreply.github.com>
2019-11-13 18:39:04 +0100

    removed unnecessary dependencies, test fails because method hashCode of Pair class is not mocked

--- app/src/test/java/com/eis0/library_demo/PollManagerTest.java
+++ app/src/test/java/com/eis0/library_demo/PollManagerTest.java
@@ -1,7 +1,6 @@
 package com.eis0.library_demo;
 
 import android.content.Context;
-import androidx.test.core.app.ApplicationProvider;
 
 import com.eis0.smslibrary.SMSMessage;
 import com.eis0.smslibrary.SMSPeer;
@@ -14,20 +13,18 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.ArrayList;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PollManagerTest {
-    private static Context fakeContext;
     private static PollManager pollManager;
 
     @Mock
+    private static Context fakeContext;
     private static PollListener fakePollListener;
 
     @BeforeClass
     public static void classSetUp() {
-        fakeContext = ApplicationProvider.getApplicationContext();
         pollManager = PollManager.getInstance(fakeContext);
         pollManager.addPollListener(fakePollListener);
     }
@@ -50,15 +47,9 @@ public class PollManagerTest {
         String text = "03337235485\r38\rQuestion\r3493619544\r3335436782\r+396662838864";
         SMSMessage message = new SMSMessage(sender, text);
         pollManager.onMessageReceived(message);
-        /* TODO: assert if onNewPollReceived() is being called (with Mockito?). If there's a way of
-         *  doing it, then remove the implementation of PollListener and its methods from this class
+        /* TODO: assert if onNewPollReceived() is being called (with Mockito?)
          */
-        verify(fakePollListener).onNewPollReceived();
-    }
 
-    /*
-    @Override
-    public void onNewPollReceived(TernaryPoll poll) {
         ArrayList<SMSPeer> users = new ArrayList<>();
         users.add(new SMSPeer("3493619544"));
         users.add(new SMSPeer("3335436782"));
@@ -66,7 +57,6 @@ public class PollManagerTest {
         TernaryPoll verificationPoll =
                 new TernaryPoll("Question", new SMSPeer("3337235485"),
                         users, 38);
-        assertThat(poll).isEqualTo(verificationPoll);
+        verify(fakePollListener).onNewPollReceived(verificationPoll);
     }
-     */
 }
