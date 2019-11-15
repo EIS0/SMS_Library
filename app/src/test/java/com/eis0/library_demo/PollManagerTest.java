32db6e12944d05cee998e5f37a9ef68bf09b5211
giovannivelludo <56405609+giovannivelludo@users.noreply.github.com>
2019-11-15 18:48:13 +0100

    removed hardcoding, added author

--- app/src/test/java/com/eis0/library_demo/PollManagerTest.java
+++ app/src/test/java/com/eis0/library_demo/PollManagerTest.java
@@ -9,7 +9,6 @@ import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.ArrayList;
-import static org.mockito.Mockito.doNothing;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -32,29 +31,35 @@ public class PollManagerTest {
     }
 
     @Test
+    /**
+     * @author Giovanni Velludo
+     */
     public void onMessageReceived() {
         // CASE 0: received new poll
         PollListener mockListener = mock(PollListener.class);
         pollManager.setPollListener(mockListener);
 
         String sep = PollManager.FIELD_SEPARATOR;
-        SMSPeer sender = new SMSPeer("3337235485");
-        String text = "0" + sep +"38" + sep + "Pizza" + sep + "Should we get takeout pizza for " +
-                "dinner?" + sep + "3493619544" + sep + "3335436782" + sep + "+396662838864";
-        SMSMessage message = new SMSMessage(sender, text);
+        int messageCode = 0;
+        SMSPeer pollAuthor = new SMSPeer("3337235485");
+        int pollId = 38;
+        String pollName = "Pizza";
+        String pollQuestion = "Should we get takeout pizza for dinner?";
+        ArrayList<SMSPeer> pollUsers = new ArrayList<>(3);
+        pollUsers.add(new SMSPeer("3493619544"));
+        pollUsers.add(new SMSPeer("3335436782"));
+        pollUsers.add(new SMSPeer("+396662838864"));
 
-        ArrayList<SMSPeer> users = new ArrayList<>();
-        users.add(new SMSPeer("3493619544"));
-        users.add(new SMSPeer("3335436782"));
-        users.add(new SMSPeer("+396662838864"));
+        String text = messageCode + sep + pollId + sep + pollName + sep + pollQuestion;
+        for (SMSPeer user : pollUsers) {
+            text += sep + user;
+        }
+        SMSMessage message = new SMSMessage(pollAuthor, text);
+
         TernaryPoll verificationPoll =
-                new TernaryPoll("Pizza", "Should we get takeout pizza for dinner?",
-                        sender, 38, users);
+                new TernaryPoll(pollName, pollQuestion, pollAuthor, pollId, pollUsers);
 
         pollManager.onMessageReceived(message);
-        /* TODO: assert if onNewPollReceived() is being called (with Mockito?)
-         */
         verify(mockListener).onIncomingPoll(verificationPoll);
-
     }
 }
