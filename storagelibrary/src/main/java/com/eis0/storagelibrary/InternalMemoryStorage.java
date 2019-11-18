195864a8427a595107851a8a996e85a2a9f35ca5
Enrico Cestaro <enrico.cestaro.3@studenti.unipd.it>
2019-11-18 13:38:14 +0100

    OK???

---
+++ storagelibrary/src/main/java/com/eis0/storagelibrary/InternalMemoryStorage.java
@@ -0,0 +1,33 @@
+package com.eis0.storagelibrary;
+
+import android.content.Context;
+
+/**
+ * Interface to implement to save, load and delete files
+ *
+ * @author Enrico Cestaro
+ */
+
+public interface InternalMemoryStorage {
+    /**
+     * Saves a .json file with the specified name
+     */
+    void saveJsonToInternal(Context context, String fileName, String json);
+
+    /**
+     * Loads the content of the .json file specified by name
+     */
+    String loadJsonFromInternal(Context context, String fileName);
+
+    /**
+     * Deletes the file specified by name
+     */
+    void deleteInternalFile(Context context, String fileName);
+
+    /**
+     * Verifies if the file exists in the Internal Storage
+     */
+    boolean doesFileExist(Context context, String fileName);
+}
+
+
