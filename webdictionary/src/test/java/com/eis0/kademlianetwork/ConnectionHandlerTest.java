950bbcb5bd7d46453976f4e493cb9310fc710c98
Cogno_Marco <marco.cognolato.98@gmail.com>
2019-12-11 14:56:57 +0100

    Improved tests with kademlia network specifications

--- webdictionary/src/test/java/com/eis0/kademlianetwork/ConnectionHandlerTest.java
+++ webdictionary/src/test/java/com/eis0/kademlianetwork/ConnectionHandlerTest.java
@@ -4,6 +4,7 @@ import com.eis0.kademlia.KademliaId;
 import com.eis0.kademlia.SMSKademliaNode;
 import com.eis0.smslibrary.SMSPeer;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
@@ -13,6 +14,14 @@ public class ConnectionHandlerTest {
     private static final SMSPeer VALID_PEER2 = new SMSPeer("+395556");
     private static final KademliaId VALID_ID = new KademliaId(VALID_PEER2);
     private static final SMSKademliaNode VALID_NODE = new SMSKademliaNode(VALID_ID, VALID_PEER2, null);
+
+    private KademliaNetwork instance;
+
+    @Before
+    public void setup(){
+        instance = KademliaNetwork.getInstance();
+        instance.init(VALID_PEER1);
+    }
 
     @Test
     public void correctRequest(){
