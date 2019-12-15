53762b5439e4ef66ff83c315276c593aa1561a13
Cogno_Marco <marco.cognolato.98@gmail.com>
2019-12-15 14:45:43 +0100

    Improved and fixed tests in KademliaBucket Improved specifications and namings

--- webdictionary/src/main/java/com/eis0/kademlia/DefaultConfiguration.java
+++ webdictionary/src/main/java/com/eis0/kademlia/DefaultConfiguration.java
@@ -10,7 +10,7 @@ public class DefaultConfiguration implements KadConfiguration {
 
     private final static int K = 2; // system-wide replication parameter
     private final static int RCSIZE = 3; //replacement cache size
-    private final static int STALE = 0;
+    private final static int MAX_STALE_COUNT = 2;
     private static final String FOLDER = "kademlia";
 
     /**
@@ -38,7 +38,7 @@ public class DefaultConfiguration implements KadConfiguration {
      * @return int representing how many time there was a stale
      */
     public int stale() {
-        return STALE;
+        return MAX_STALE_COUNT;
     }
 
 }
