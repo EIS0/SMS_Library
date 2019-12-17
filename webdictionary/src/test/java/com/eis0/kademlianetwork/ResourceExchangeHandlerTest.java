f8c23f13a8ea4813e8b15f65f4be8b652f4d70fb
Enrico Cestaro <enrico.cestaro.3@studenti.unipd.it>
2019-12-17 17:30:23 +0100

    Testclass

--- webdictionary/src/test/java/com/eis0/kademlianetwork/ResourceExchangeHandlerTest.java
+++ webdictionary/src/test/java/com/eis0/kademlianetwork/ResourceExchangeHandlerTest.java
@@ -1,5 +1,29 @@
+package com.eis0.kademlianetwork;
+
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class ResourceExchangeHandlerTest {
 
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void createAddRequest() {
+    }
+
+    @Test
+    public void processRequest() {
+    }
+
+    @Test
+    public void completeAddRequest() {
+    }
+
+    @Test
+    public void createGetRequest() {
+    }
 }
