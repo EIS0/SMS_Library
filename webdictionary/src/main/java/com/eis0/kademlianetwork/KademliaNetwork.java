7985644924c07792c75a1a9a511969a0a3464717
Marco Cognolato <marco.cognolato.98@gmail.com>
2020-01-03 18:12:21 +0100

    Test Fixes: + Imported missing Timer in KademliaNetwork + Added test in IdFinderHandlerTest ~ Fixed test in IdFinderHandlerTest

--- webdictionary/src/main/java/com/eis0/kademlianetwork/KademliaNetwork.java
+++ webdictionary/src/main/java/com/eis0/kademlianetwork/KademliaNetwork.java
@@ -10,6 +10,7 @@ import com.eis0.kademlia.KademliaId;
 import com.eis0.kademlia.SMSKademliaNode;
 import com.eis0.kademlia.SMSKademliaRoutingTable;
 import com.eis0.kademlianetwork.ActivityStatus.NodeConnectionInfo;
+import com.eis0.kademlianetwork.ActivityStatus.RespondTimer;
 import com.eis0.kademlianetwork.RoutingTableManager.RoutingTableRefresh;
 import com.eis0.kademlianetwork.RoutingTableManager.TableUpdateHandler;
 import com.eis0.webdictionary.SMSNetVocabulary;
