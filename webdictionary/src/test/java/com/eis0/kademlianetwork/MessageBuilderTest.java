0ae1fd528fd57b7ccefcfa57ec245bdd1ad3b2c1
Marco Cognolato <marco.cognolato.98@gmail.com>
2020-01-07 22:00:51 +0100

    Improved Tests + Added tests in MessageBuilder ~ Improved old tests parametrization

--- webdictionary/src/test/java/com/eis0/kademlianetwork/MessageBuilderTest.java
+++ webdictionary/src/test/java/com/eis0/kademlianetwork/MessageBuilderTest.java
@@ -11,66 +11,77 @@ import static org.junit.Assert.assertEquals;
 public class MessageBuilderTest {
 
     private final SMSPeer VALID_PEER = new SMSPeer("+393423541601");
+    private final String BLANK = "/";
+    private final String FAKE_ID = "ABCDEF0123456789";
+    private final String EXPECTED_STRING = FAKE_ID + " " + BLANK + " " + BLANK;
 
     @Test
     public void buildMessage_everythingCorrect() {
-        SMSMessage expectedMessage = new SMSMessage(VALID_PEER,
-                        "ABCDEF0123456789 / /"
-        );
+        SMSMessage expectedMessage = new SMSMessage(VALID_PEER, EXPECTED_STRING);
         SMSMessage actualMessage = new MessageBuilder()
                 .setPeer(VALID_PEER)
-                .addArguments("ABCDEF0123456789", "/", "/")
+                .addArguments(FAKE_ID, BLANK, BLANK)
                 .buildMessage();
         assertEquals(actualMessage, expectedMessage);
     }
 
     @Test
     public void buildMessage_nullValues() {
-        SMSMessage expectedMessage = new SMSMessage(VALID_PEER,
-                        "ABCDEF0123456789 / /"
-        );
+        SMSMessage expectedMessage = new SMSMessage(VALID_PEER, EXPECTED_STRING);
         SMSMessage actualMessage = new MessageBuilder()
                 .setPeer(VALID_PEER)
-                .addArguments("ABCDEF0123456789", null, "/")
+                .addArguments(FAKE_ID, null, BLANK)
                 .buildMessage();
         assertEquals(actualMessage, expectedMessage);
     }
 
     @Test
     public void buildMessage_emptyValues() {
-        SMSMessage expectedMessage = new SMSMessage(VALID_PEER,
-                        "ABCDEF0123456789 / /"
-        );
+        SMSMessage expectedMessage = new SMSMessage(VALID_PEER, EXPECTED_STRING);
         SMSMessage actualMessage = new MessageBuilder()
                 .setPeer(VALID_PEER)
-                .addArguments("ABCDEF0123456789", "", "/")
+                .addArguments(FAKE_ID, "", BLANK)
                 .buildMessage();
         assertEquals(actualMessage, expectedMessage);
     }
 
     @Test
     public void buildMessage_multiplePasses() {
-        SMSMessage expectedMessage = new SMSMessage(VALID_PEER,
-                        "ABCDEF0123456789 / /"
-        );
+        SMSMessage expectedMessage = new SMSMessage(VALID_PEER, EXPECTED_STRING);
         SMSMessage actualMessage = new MessageBuilder()
                 .setPeer(VALID_PEER)
-                .addArguments("ABCDEF0123456789")
-                .addArguments("", "/")
+                .addArguments(FAKE_ID)
+                .addArguments("", BLANK)
                 .buildMessage();
         assertEquals(actualMessage, expectedMessage);
     }
 
     @Test(expected = IllegalStateException.class)
     public void buildMessage_noPeerError() {
-        SMSMessage actualMessage = new MessageBuilder()
-                .buildMessage();
+        new MessageBuilder().buildMessage();
     }
 
     @Test(expected = IllegalStateException.class)
     public void buildMessage_noMessageError() {
-        SMSMessage actualMessage = new MessageBuilder()
-                .setPeer(VALID_PEER)
-                .buildMessage();
+        new MessageBuilder().setPeer(VALID_PEER).buildMessage();
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void resetBuilding_noPeerError(){
+        new MessageBuilder().setPeer(VALID_PEER).addArguments(FAKE_ID)
+                .reset().buildMessage();
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void resetBuilding_noMessageError(){
+        new MessageBuilder().setPeer(VALID_PEER).addArguments(FAKE_ID)
+                .reset().setPeer(VALID_PEER).buildMessage();
+    }
+
+    @Test()
+    public void resetBuilding_fullMessage(){
+        new MessageBuilder().setPeer(VALID_PEER).addArguments(FAKE_ID)
+                .reset().setPeer(VALID_PEER)
+                .addArguments(FAKE_ID, BLANK, BLANK).buildMessage();
     }
 }
