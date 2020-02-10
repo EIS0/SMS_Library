f787122eb2b9b2fee133ede94b4613f5535bf035
Edoardo Raimondi <edoardo.raimondi@studenti.unipd.it>
2020-02-10 15:46:33 +0100

    Revert "Change some public methods to private"
    This reverts commit 2efc91aeb461d142822581e0690d30ddd89fd45a.

--- webdictionary/src/main/java/com/eis0/kademlianetwork/informationdeliverymanager/ResourceExchangeHandler.java
+++ webdictionary/src/main/java/com/eis0/kademlianetwork/informationdeliverymanager/ResourceExchangeHandler.java
@@ -136,7 +136,7 @@ public class ResourceExchangeHandler {
      *
      * @return The pendingAddRequests map of the ResourceExchangeHandler class
      */
-    private Map<KademliaId, Request> getPendingAddRequests() {
+    public Map<KademliaId, Request> getPendingAddRequests() {
         return pendingAddRequests;
     }
 
@@ -145,7 +145,7 @@ public class ResourceExchangeHandler {
      *
      * @return The pendingGetRequests map of the ResourceExchangeHandler class
      */
-    private Map<KademliaId, Request> getPendingGetRequests() {
+    public Map<KademliaId, Request> getPendingGetRequests() {
         return pendingGetRequests;
     }
 
@@ -154,7 +154,7 @@ public class ResourceExchangeHandler {
      *
      * @return The pendingGetRequests map of the ResourceExchangeHandler class
      */
-    private Map<KademliaId, Request> getPendingDeleteRequests() {
+    public Map<KademliaId, Request> getPendingDeleteRequests() {
         return pendingDeleteRequests;
     }
 }
