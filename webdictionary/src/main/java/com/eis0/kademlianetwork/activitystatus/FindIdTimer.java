26e9aceec23cac0ad3c88a720f3446ae1250674e
Marco Cognolato <marco.cognolato.98@gmail.com>
2020-02-11 12:09:25 +0100

    feat: implemented part of the FindResource command feat: added a KademliaFailReason feat: improved FindIdTimer, now stops if the FindIdRequest is completed

--- webdictionary/src/main/java/com/eis0/kademlianetwork/activitystatus/FindIdTimer.java
+++ webdictionary/src/main/java/com/eis0/kademlianetwork/activitystatus/FindIdTimer.java
@@ -1,6 +1,10 @@
 package com.eis0.kademlianetwork.activitystatus;
 
+import androidx.annotation.NonNull;
+
 import com.eis0.kademlia.KademliaId;
+import com.eis0.kademlianetwork.informationdeliverymanager.FindIdRequest;
+
 import java.util.TimerTask;
 
 /**
@@ -8,12 +12,17 @@ import java.util.TimerTask;
  *
  * @author Marco Cognolato
  */
-class FindIdTimer extends TimerTask {
+public class FindIdTimer extends TimerTask {
 
     private static final int ONE_SECOND = 1000;
     private static final int NUMBER_OF_SECONDS = KademliaId.ID_LENGTH;
 
     private int currentSecond = 0;
+    private FindIdRequest request;
+
+    public FindIdTimer(@NonNull FindIdRequest request){
+        this.request = request;
+    }
 
     @Override
     public void run() {
@@ -21,12 +30,12 @@ class FindIdTimer extends TimerTask {
     }
 
     /**
-     * Waits for at most {@link #NUMBER_OF_SECONDS} seconds.
+     * Waits for at most {@link #NUMBER_OF_SECONDS} seconds, or for the FindIdRequest to finish
      */
     private void completeTask() {
         try {
             //assuming it takes 10 secs to complete the task
-            while (currentSecond < NUMBER_OF_SECONDS) {
+            while (currentSecond < NUMBER_OF_SECONDS && !request.isCompleted()) {
                 Thread.sleep(ONE_SECOND);
                 currentSecond++;
             }
--- webdictionary/src/main/java/com/eis0/kademlianetwork/commands/networkdictionary/FindResource.java
+++ webdictionary/src/main/java/com/eis0/kademlianetwork/commands/networkdictionary/FindResource.java
@@ -4,17 +4,23 @@ import androidx.annotation.NonNull;
 
 import com.eis.smslibrary.SMSPeer;
 import com.eis0.kademlia.KademliaId;
+import com.eis0.kademlianetwork.KademliaFailReason;
 import com.eis0.kademlianetwork.KademliaJoinableNetwork;
+import com.eis0.kademlianetwork.activitystatus.FindIdTimer;
+import com.eis0.kademlianetwork.informationdeliverymanager.FindIdRequest;
 import com.eis0.kademlianetwork.informationdeliverymanager.IdFinderHandler;
-import com.eis0.kademlianetwork.informationdeliverymanager.ResourceRequest;
-import com.eis0.kademlianetwork.informationdeliverymanager.ResearchMode;
 import com.eis0.kademlianetwork.informationdeliverymanager.RequestsHandler;
+import com.eis0.kademlianetwork.informationdeliverymanager.ResearchMode;
+import com.eis0.kademlianetwork.informationdeliverymanager.ResourceRequest;
 import com.eis0.netinterfaces.commands.Command;
 
 public class FindResource extends Command {
 
     protected final String key;
     private RequestsHandler requestsHandler;
+
+    private String resource;
+    private KademliaFailReason failReason;
 
     /**
      * Constructor for the FindResource command
@@ -28,16 +34,50 @@ public class FindResource extends Command {
     }
 
     /**
-     * Finds a node closest to a resource from the network dictionary
+     * Finds a resource from the network dictionary
      *
      * @see {@link RequestsHandler} for more details
      */
     public void execute() {
+        //Finding a resource goes as follows:
+        //1. Start a new ResourceRequest
         ResourceRequest currentResourceRequest = new ResourceRequest(key, null);
+        //requestsHandler.getPendingFindRequests().put(idToFind, currentResourceRequest);
+
+        //2. Search the closest id to the resource you're searching
         KademliaId idToFind = currentResourceRequest.getKeyId();
-        requestsHandler.getPendingFindRequests().put(idToFind, currentResourceRequest);
-        //Starts to search for the closest ID
+        FindIdRequest findIdRequest = requestsHandler.startFindIdRequest(idToFind);
         SMSPeer searcher = KademliaJoinableNetwork.getInstance().getLocalNode().getPeer();
         IdFinderHandler.searchId(idToFind, searcher, ResearchMode.FindInDictionary);
+
+        //3. wait for the findIdRequest to finish
+        new FindIdTimer(findIdRequest).run();
+
+        //if I'm here it means that either the request took too long, or that I have found the
+        //closest person to the id to find
+        if(!findIdRequest.isCompleted()){
+            failReason = KademliaFailReason.REQUEST_EXPIRED;
+            return;
+        }
+
+        //if I'm here it means that I've found the closest person to the id to find
+        //4. Ask that person for the resource
+        //5. Wait for that person to answer
+    }
+
+    /**
+     * @return Return the resource found by the FindResource Command. If no resource was found
+     * null is returned instead.
+     */
+    public String getResource(){
+        return resource;
+    }
+
+    /**
+     * @return Returns a KademliaFailReason for this request. If the Request
+     * completed successfully, or is still going, null is returned.
+     */
+    public KademliaFailReason getFailReason(){
+        return failReason;
     }
 }
