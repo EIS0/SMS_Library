4ff678dbb3ccb85cac4b9841f14e173354af72d6
Edoardo Raimondi <edoardo.raimondi@studenti.unipd.it>
2020-02-11 14:40:25 +0100

    Added KadAddLocalResource test class

---
+++ webdictionary/src/test/java/com/eis0/kademlianetwork/commands/localdictionary/KadAddLocalResourceTest.java
@@ -0,0 +1,19 @@
+package com.eis0.kademlianetwork.commands.localdictionary;
+
+import com.eis0.netinterfaces.NetDictionary;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class KadAddLocalResourceTest {
+
+    NetDictionary<String, String> test;
+
+
+    @Test
+    public void execute() {
+        new KadAddLocalResource("test", "", test).execute();
+        assertEquals(test.getResource("test"), "");
+    }
+}
