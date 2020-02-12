4c7dea94d53606d69b8eaf8e29941071ce79c951
Edoardo Raimondi <edoardo.raimondi@studenti.unipd.it>
2020-02-12 12:51:16 +0100

    RequestHandler tested
    Methods are untestable for an alph

--- webdictionary/src/test/java/com/eis0/kademlianetwork/RequestsHandlerTest.java
+++ webdictionary/src/test/java/com/eis0/kademlianetwork/RequestsHandlerTest.java
@@ -6,7 +6,10 @@ import com.eis.smslibrary.SMSPeer;
 import com.eis0.UtilityMocks;
 import com.eis0.kademlia.KademliaId;
 import com.eis0.kademlia.SMSKademliaNode;
+import com.eis0.kademlianetwork.informationdeliverymanager.AddResourceRequest;
 import com.eis0.kademlianetwork.informationdeliverymanager.DeleteResourceRequest;
+import com.eis0.kademlianetwork.informationdeliverymanager.FindIdRequest;
+import com.eis0.kademlianetwork.informationdeliverymanager.FindResourceRequest;
 import com.eis0.kademlianetwork.informationdeliverymanager.RequestsHandler;
 import com.eis0.kademlianetwork.listener.SMSKademliaListener;
 
@@ -20,65 +23,42 @@ import org.powermock.modules.junit4.PowerMockRunner;
 import java.util.Map;
 
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(SmsManager.class)
 public class RequestsHandlerTest {
-    private RequestsHandler requestsHandler;
+
+    private RequestsHandler requestsHandler = new RequestsHandler();
     private final String KEY1 = "Key1";
-    private final String KEY2 = "Key1";
     private final String RESOURCE1 = "First resource";
-    private final String RESOURCE2 = "Second resource";
     private final KademliaId KAD_ID1 = new KademliaId(KEY1);
-    private final KademliaId KAD_ID2 = new KademliaId(KEY2);
-
-    private final SMSPeer VALID_PEER = new SMSPeer("+393423541601");
-    private final SMSKademliaNode MAIN_NODE = new SMSKademliaNode(VALID_PEER);
-
-    private final SMSKademliaListener mockListener = mock(SMSKademliaListener.class);
-    private KademliaNetwork kadNet;
-    private DeleteResourceRequest resourceRequest1;
-
-    @Before
-    public void setUp() {
-        requestsHandler = new RequestsHandler();
-
-        /*
-        resourceRequest1 = mock(RequestsHandler.DeleteResourceRequest.class);
-        when(resourceRequest1.getKey()).thenReturn(KEY1);
-        when(resourceRequest1.getKeyId()).thenReturn(KAD_ID1);
-        String resource1 = "Resource1";
-        when(resourceRequest1.getResource()).thenReturn(resource1);
-
-         */
-
-        DeleteResourceRequest resourceRequest2 = mock(DeleteResourceRequest.class);
-        when(resourceRequest2.getKey()).thenReturn(KEY2);
-        when(resourceRequest2.getKeyId()).thenReturn(KAD_ID2);
-
-        SmsManager smsManagerMock = mock(SmsManager.class);
-
-        PowerMockito.mockStatic(SmsManager.class);
-        PowerMockito.when(SmsManager.getDefault()).thenReturn(smsManagerMock);
-        //when(smsManagerMock.sendTextMessage(any(String.class), any(String.class), any(String.class), any(PendingIntent.class), any(PendingIntent.class)));
 
 
-        kadNet = KademliaJoinableNetwork.getInstance();
-        kadNet.init(MAIN_NODE, UtilityMocks.setupMocks());
-        when(kadNet.isAlive(MAIN_NODE.getPeer())).thenReturn(true);
-        //requestsHandler.createAddRequest(KEY2, resource2);
+    @Test
+    public void startFindIdRequest(){
+        FindIdRequest request = new FindIdRequest(KAD_ID1);
+        assertTrue(requestsHandler.startFindIdRequest(KAD_ID1).equals(request));
     }
 
     @Test
-    public void createAddRequest_equals() {
-        /*
-        requestsHandler.createRequest(KEY1, RESOURCE1);
-        Map<KademliaId, RequestsHandler.DeleteResourceRequest> addRequests = requestsHandler.getPendingAddRequests();
-        resourceRequest1 = addRequests.get(KAD_ID1);
-        assertEquals(resourceRequest1.getKeyId(), KAD_ID1);
-
-         */
+    public void startFindResourceRequest(){
+        FindResourceRequest request = new FindResourceRequest(RESOURCE1);
+        assertTrue(requestsHandler.startFindResourceRequest(RESOURCE1).equals(request));
     }
+
+    @Test
+    public void startAddResourceRequest(){
+        AddResourceRequest resourceRequest = new AddResourceRequest(KEY1, RESOURCE1);
+        assertTrue(requestsHandler.startAddResourceRequest(KEY1, RESOURCE1).equals(resourceRequest));
+    }
+
+    @Test
+    public void startDeleteResourceRequest(){
+        DeleteResourceRequest resourceRequest = new DeleteResourceRequest(KEY1);
+        assertTrue(requestsHandler.startDeleteResourceRequest(KEY1).equals(resourceRequest));
+    }
+
 }
