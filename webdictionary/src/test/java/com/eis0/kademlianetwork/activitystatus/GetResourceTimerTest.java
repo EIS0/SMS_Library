7b30321dbf24c9cd59c5cefaea004c99455f7d35
Cogno_Marco <marco.cognolato.98@gmail.com>
2020-02-23 17:54:07 +0100

    test: tested basic timer functionality for FindId and GetResource timers

--- webdictionary/src/test/java/com/eis0/kademlianetwork/activitystatus/GetResourceTimerTest.java
+++ webdictionary/src/test/java/com/eis0/kademlianetwork/activitystatus/GetResourceTimerTest.java
@@ -1,7 +1,30 @@
 package com.eis0.kademlianetwork.activitystatus;
 
-import static org.junit.Assert.*;
+import com.eis0.kademlianetwork.informationdeliverymanager.Requests.FindResourceRequest;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class GetResourceTimerTest {
 
+    private FindResourceRequest requestMock;
+
+    @Before
+    public void setup(){
+        requestMock = mock(FindResourceRequest.class);
+        //setting up the mock to return false, then true.
+        when(requestMock.isCompleted()).thenReturn(false).thenReturn(true);
+    }
+
+    @Test
+    public void requestCompleted_waitsForOneSecond(){
+        GetResourceTimer timer = new GetResourceTimer(requestMock);
+        timer.run();
+        verify(requestMock, times(2)).isCompleted();
+    }
 }
