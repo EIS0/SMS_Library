d1e6625cf05327e64a6cc15037de4d197bf3c256
Cogno_Marco <marco.cognolato.98@gmail.com>
2020-02-24 15:08:47 +0100

    test: tested AcceptJoin test: tested FindId

--- webdictionary/src/test/java/com/eis0/kademlianetwork/SMSKademliaListenerTest.java
+++ webdictionary/src/test/java/com/eis0/kademlianetwork/SMSKademliaListenerTest.java
@@ -1,10 +1,14 @@
 package com.eis0.kademlianetwork;
 
+import android.util.Log;
+
 import com.eis.smslibrary.SMSManager;
 import com.eis.smslibrary.SMSMessage;
 import com.eis.smslibrary.SMSPeer;
 import com.eis0.UtilityMocks;
+import com.eis0.kademlia.KademliaId;
 import com.eis0.kademlia.SMSKademliaNode;
+import com.eis0.kademlianetwork.informationdeliverymanager.IdFinderHandler;
 import com.eis0.kademlianetwork.informationdeliverymanager.KademliaMessage;
 import com.eis0.kademlianetwork.informationdeliverymanager.RequestTypes;
 import com.eis0.kademlianetwork.listener.SMSKademliaListener;
@@ -24,11 +28,13 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({KademliaNetwork.class, SMSManager.class, KademliaJoinableNetwork.class})
+@PrepareForTest({KademliaNetwork.class, SMSManager.class,
+        KademliaJoinableNetwork.class, Log.class, IdFinderHandler.class})
 public class SMSKademliaListenerTest {
 
     private final SMSPeer peer1 = new SMSPeer("+393423541601");
-
+    private final SMSPeer peer2 = new SMSPeer("+393423541602");
+    private final KademliaId idToFind1 = new KademliaId(peer2);
     private final SMSKademliaNode node1 = new SMSKademliaNode(peer1);
 
     private SMSManager smsManagerMock;
@@ -56,14 +62,26 @@ public class SMSKademliaListenerTest {
             .setPeer(peer1)
             .setRequestType(RequestTypes.JoinPermission)
             .buildMessage();
+    private final SMSMessage acceptJoinMessage = new KademliaMessage()
+            .setPeer(peer1)
+            .setRequestType(RequestTypes.AcceptJoin)
+            .buildMessage();
+    private final SMSMessage findIdMessage = new KademliaMessage()
+            .setPeer(peer1)
+            .setIdToFind(idToFind1)
+            .setSearcher(peer1)
+            .setRequestType(RequestTypes.FindId)
+            .buildMessage();
+
     @Before
     public void setUp() {
-
         smsManagerMock = mock(SMSManager.class);
         PowerMockito.mockStatic(SMSManager.class);
         PowerMockito.when(SMSManager.getInstance()).thenReturn(smsManagerMock);
         PowerMockito.mockStatic(KademliaJoinableNetwork.class);
         PowerMockito.when(KademliaJoinableNetwork.getInstance()).thenReturn(spyNetwork);
+        PowerMockito.mockStatic(Log.class);
+        PowerMockito.mockStatic(IdFinderHandler.class);
 
         doCallRealMethod().when(spyListener).onMessageReceived(acknowledgeMessage);
         spyNetwork.init(node1, UtilityMocks.setupMocks());
@@ -94,6 +112,23 @@ public class SMSKademliaListenerTest {
     public void JoinPermission_asExpected() {
         spyListener.onMessageReceived(joinMessage);
         verify(spyNetwork, times(1)).checkInvitation(new KademliaInvitation(peer1));
+    }
+
+    @Test
+    public void AcceptJoin_asExpected(){
+        spyListener.onMessageReceived(acceptJoinMessage);
+        verify(spyNetwork, times(1))
+                .addNodeToTable(new SMSKademliaNode(peer1));
+        verify(spyNetwork, times(1))
+                .updateTable();
+    }
+
+    @Test
+    public void FindId_asExpected(){
+        spyListener.onMessageReceived(findIdMessage);
+        verify(smsManagerMock).sendMessage(acknowledgeMessage);
+        PowerMockito.verifyStatic(times(1));
+        IdFinderHandler.searchId(idToFind1, peer1);
     }
 
 }
