e2d05097df4c7d42e05638af088fbddac2113d96
Cogno_Marco <marco.cognolato.98@gmail.com>
2020-02-24 14:40:03 +0100

    fix: implemented hashCode and isEquals for KademliaInvitation test(KademliaInvitation): tested isEquals method test(SMSKademliaListener): tested AcceptJoin request style(SMSKademliaListener): formatted code and changed some variable names

--- webdictionary/src/test/java/com/eis0/kademlianetwork/KademliaInvitationTest.java
+++ webdictionary/src/test/java/com/eis0/kademlianetwork/KademliaInvitationTest.java
@@ -8,11 +8,33 @@ import static org.junit.Assert.*;
 
 public class KademliaInvitationTest {
 
-     private final SMSPeer PEER = new SMSPeer("1234567");
-     private final KademliaInvitation INVITATION = new KademliaInvitation(PEER);
+    private final SMSPeer PEER1 = new SMSPeer("+393423541601");
+    private final SMSPeer PEER2 = new SMSPeer("+393423541602");
+    private final KademliaInvitation INVITATION1 = new KademliaInvitation(PEER1);
+    private final KademliaInvitation INVITATION2 = new KademliaInvitation(PEER2);
 
     @Test
     public void getInviterPeer() {
-         assertEquals(INVITATION.getInviterPeer(), PEER);
+         assertEquals(INVITATION1.getInviterPeer(), PEER1);
+    }
+
+    @Test
+    public void isEqual_sameObject_isEqual(){
+        assertEquals(INVITATION1, INVITATION1);
+    }
+
+    @Test
+    public void isEqual_differentInvitations_areDifferent(){
+        assertNotEquals(INVITATION1, INVITATION2);
+    }
+
+    @Test
+    public void isEqual_differentObjects_areDifferent(){
+        assertNotEquals(INVITATION1, "a different object");
+    }
+
+    @Test
+    public void isEqual_nullObject_isDifferent(){
+        assertNotEquals(INVITATION1, null);
     }
 }
--- webdictionary/src/test/java/com/eis0/kademlianetwork/SMSKademliaListenerTest.java
+++ webdictionary/src/test/java/com/eis0/kademlianetwork/SMSKademliaListenerTest.java
@@ -20,13 +20,14 @@ import org.powermock.modules.junit4.PowerMockRunner;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.doCallRealMethod;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({KademliaNetwork.class, SMSManager.class})
+@PrepareForTest({KademliaNetwork.class, SMSManager.class, KademliaJoinableNetwork.class})
 public class SMSKademliaListenerTest {
 
-    private final SMSPeer peer1 = new SMSPeer("+3408140326");
+    private final SMSPeer peer1 = new SMSPeer("+393423541601");
 
     private final SMSKademliaNode node1 = new SMSKademliaNode(peer1);
 
@@ -39,52 +40,60 @@ public class SMSKademliaListenerTest {
     private SMSKademliaListener spyListener = new SMSKademliaListener();
 
     /*Default messages*/
-    private final SMSMessage acknowledge = new KademliaMessage()
+    private final SMSMessage acknowledgeMessage = new KademliaMessage()
             .setPeer(peer1)
             .setRequestType(RequestTypes.AcknowledgeMessage)
             .buildMessage();
-    private final SMSMessage ping = new KademliaMessage()
+    private final SMSMessage pingMessage = new KademliaMessage()
             .setPeer(peer1)
             .setRequestType(RequestTypes.Ping)
             .buildMessage();
-    private final SMSMessage pong = new KademliaMessage()
+    private final SMSMessage pongMessage = new KademliaMessage()
             .setPeer(peer1)
             .setRequestType(RequestTypes.Pong)
             .buildMessage();
-
+    private final SMSMessage joinMessage = new KademliaMessage()
+            .setPeer(peer1)
+            .setRequestType(RequestTypes.JoinPermission)
+            .buildMessage();
     @Before
-    public void setUp(){
-
-        spyNetwork.init(node1, UtilityMocks.setupMocks());
+    public void setUp() {
 
         smsManagerMock = mock(SMSManager.class);
         PowerMockito.mockStatic(SMSManager.class);
         PowerMockito.when(SMSManager.getInstance()).thenReturn(smsManagerMock);
+        PowerMockito.mockStatic(KademliaJoinableNetwork.class);
+        PowerMockito.when(KademliaJoinableNetwork.getInstance()).thenReturn(spyNetwork);
 
-        doCallRealMethod().when(spyListener).onMessageReceived(acknowledge);
+        doCallRealMethod().when(spyListener).onMessageReceived(acknowledgeMessage);
+        spyNetwork.init(node1, UtilityMocks.setupMocks());
     }
 
-
-
     @Test
-    public void AcknowledgeAction(){
-        //I suppose someone sent me an acknowledge message
-        spyListener.onMessageReceived(acknowledge);
+    public void AcknowledgeAction_asExpected() {
+        //I suppose someone sent me an acknowledgeMessage message
+        spyListener.onMessageReceived(acknowledgeMessage);
         assertTrue(spyNetwork.connectionInfo.hasRespond());
     }
 
     @Test
-    public void PingAction(){
-        //I suppose someone sent me a ping message
-        spyListener.onMessageReceived(ping);
-        verify(smsManagerMock).sendMessage(pong);
+    public void PingAction_asExpected() {
+        //I suppose someone sent me a pingMessage message
+        spyListener.onMessageReceived(pingMessage);
+        verify(smsManagerMock).sendMessage(pongMessage);
     }
 
     @Test
-    public void PongAction(){
-        //I suppose someone sent me a pong message
-        spyListener.onMessageReceived(pong);
+    public void PongAction_asExpected() {
+        //I suppose someone sent me a pongMessage message
+        spyListener.onMessageReceived(pongMessage);
         assertTrue(spyNetwork.connectionInfo.hasPong());
+    }
+
+    @Test
+    public void JoinPermission_asExpected() {
+        spyListener.onMessageReceived(joinMessage);
+        verify(spyNetwork, times(1)).checkInvitation(new KademliaInvitation(peer1));
     }
 
 }
